372044fd0fb6c849b49df242d1c9f982c80025c8
pedro3pv <53156040+pedro3pv@users.noreply.github.com>
2022-11-27 11:10:09 -0300

    Revert "consertar"
    This reverts commit 1a99751d956155fb88526cb3ddcd5d8f4e929d9e.

--- js/Mata-Mata.js
+++ js/Mata-Mata.js
@@ -1,7 +1,6 @@
 let vencedores = []
 
 let entrada = []
-<<<<<<< HEAD
 
 let quartas = []
 
@@ -10,25 +9,10 @@ let jogo = []
 let cont = 0
 let cont2 = 0
 
-=======
-let entrada2 = []
-
-let quartas = []
-
-let j = 0
-
-let sequencia = [0,3,4,7,6,5,2,1,8,11,12,15,10,9,14,13]
-let sequencia2 = ["1A","2A","1B","2B","1C","2C","1D","2D","1E","2E","1F","2F","1G","2G","1H","2H"]
-let temp = []
-let temp2 = []
-let aux = 0
-let aux2 = 0
->>>>>>> parent of 928cbb9 (javascript pronto)
 // 0 3 4 7 6 5 2 1 8 11 12 15 10 9 14 13
 
 for (let u = 0; u <= 15; u++) {
         vencedores[u] = localStorage.getItem("vencedores[" + u + "]")
-<<<<<<< HEAD
 }
 document.getElementById("1A").innerHTML = vencedores[0]
 document.getElementById("2A").innerHTML = vencedores[1]
@@ -47,42 +31,12 @@ document.getElementById("2G").innerHTML = vencedores[13]
 document.getElementById("1H").innerHTML = vencedores[14]
 document.getElementById("2H").innerHTML = vencedores[15]
 
-=======
-        entrada2[u] = sessionStorage.getItem("oitavas"+u)
-        document.getElementById("O"+u).value = entrada2[u]
-        if(sequencia[u] % 2 == 0){
-                temp[aux] = sequencia[u]
-                aux++
-        }else if(sequencia[u] % 2 != 0){
-                temp2[aux2] = sequencia[u]
-                aux2++
-        }
-}
-
-for (let z = 0; z <= 7; z++) {verificador(temp[z],temp2[z],z)}
-
-document.oninput = function () {for (let z = 0; z <= 7; z++) {verificador(temp[z],temp2[z],z)}}
-
-for (let g = 0; g <= 15; g++) document.getElementById(sequencia2[g]).innerHTML = vencedores[g]
-
-function verificador(x, y, j) {
-        enter()
-        if (entrada[x] > entrada[y] && entrada[y] != "" && entrada[x] != "") {
-                quartas[j] = vencedores[x]
-                document.getElementById("Q"+j+"T").innerHTML = quartas[j]
-        } else if (entrada[x] < entrada[y] && entrada[y] != "" && entrada[x] != "") {
-                quartas[j] = vencedores[y]
-                document.getElementById("Q"+j+"T").innerHTML = quartas[j]
-        }
-}
->>>>>>> parent of 928cbb9 (javascript pronto)
 function enter() {
         for (let q = 0; q <= 15; q++) {
                 if (document.getElementById("O" + q).value != null) {
                         entrada[q] = document.getElementById("O" + q).value
                 }
         }
-<<<<<<< HEAD
 }
 
 document.getElementById("O0").oninput = function () {
@@ -182,10 +136,5 @@ function verificador(x, y, j) {
                 document.getElementById("Q"+g+"T").innerHTML = quartas[g]
                         }
                 }
-=======
-        for(let h = 0;h <= 15;h++){
-                        sessionStorage.setItem("oitavas" + h, parseInt(entrada2[h]))
-                        entrada[sequencia[h]] = parseInt(entrada2[h])
->>>>>>> parent of 928cbb9 (javascript pronto)
         }
 }
