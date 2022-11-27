2839279f7e6cd5a42c41f1cc54ff5e67bc83bcb0
pedro3pv <53156040+pedro3pv@users.noreply.github.com>
2022-11-27 10:50:02 -0300

    Revert "Merge branch 'main' of https://github.com/pedro3pv/ProjetoDAVI"
    This reverts commit 869fe3e82a787166e8a7b22d806de14f82f1aaeb, reversing
    changes made to 92399302ac88207061fc36aec8673c9d8220c3d4.

--- js/Mata-Mata.js
+++ js/Mata-Mata.js
@@ -1,148 +1,140 @@
 let vencedores = []
 
 let entrada = []
-let entrada2 = []
-let entradaQ = []
-let entradaS = []
-let entradaT = []
-let entradaF = []
 
 let quartas = []
-let semifinal = []
-let lugar3 = []
-let final = []
 
-let sequencia = [0,3,4,7,6,5,2,1,8,11,12,15,10,9,14,13]
-let sequencia2 = ["1A","2A","1B","2B","1C","2C","1D","2D","1E","2E","1F","2F","1G","2G","1H","2H"]
-let divisao = []
-let divisao2 = []
-let aux = 0
-let aux2 = 0
+let jogo = []
+
+let cont = 0
+let cont2 = 0
+
 // 0 3 4 7 6 5 2 1 8 11 12 15 10 9 14 13
 
 for (let u = 0; u <= 15; u++) {
         vencedores[u] = localStorage.getItem("vencedores[" + u + "]")
-        document.getElementById(sequencia2[u]).innerHTML = vencedores[u]
-        entrada2[u] = sessionStorage.getItem("oitavas"+u)
-        document.getElementById("O"+u).value = entrada2[u]
-        if(sequencia[u] % 2 == 0){
-                divisao[aux] = sequencia[u]
-                aux++
-        }else if(sequencia[u] % 2 != 0){
-                divisao2[aux2] = sequencia[u]
-                aux2++
-        }
 }
-for (let z = 0; z <= 7; z++) {
-        entradaQ[z] = sessionStorage.getItem("quartas"+z)
-        document.getElementById("Q"+z).value = entradaQ[z]
-}
-for (let z = 0; z < 4; z++) {
-        entradaS[z] = sessionStorage.getItem("terceiro"+z)
-        document.getElementById("S"+z).value = entradaS[z]
-}
-for (let z = 0; z < 2; z++) {
-        entradaT[z] = sessionStorage.getItem("terceiroE"+z)
-        document.getElementById("T"+z).value = entradaT[z]
-}
-for (let z = 0; z < 2; z++) {
-        entradaF[z] = sessionStorage.getItem("final"+z)
-        document.getElementById("F"+z).value = entradaF[z]
-}
-
-for (let z = 0; z <= 7; z++) {verificador(divisao[z],divisao2[z],z)}
-let contadorJ = 0
-for (let z = 0; z < 8; z = z + 2) {
-        verificadorQ(z,z+1,contadorJ)
-        contadorJ++
-}
-let contadorJ2 = 0
-for (let z = 0; z < 4; z = z + 2) {
-        verificadorS(z,z+1,contadorJ2)
-        contadorJ2++
-}
-
-document.oninput = function () {
-        for (let z = 0; z <= 7; z++) {
-                verificador(divisao[z],divisao2[z],z)
-        }
-        let contadorJ = 0
-        for (let z = 0; z < 8; z = z + 2) {
-                verificadorQ(z,z+1,contadorJ)
-                contadorJ++
-        }
-        let contadorJ2 = 0
-        for (let z = 0; z < 4; z = z + 2) {
-                verificadorS(z,z+1,contadorJ2)
-                contadorJ2++
-        }
-}
-
-function verificador(x, y, j) {
-        enter()
-        if (entrada[x] > entrada[y] && entrada[y] != "" && entrada[x] != "") {
-                quartas[j] = vencedores[x]
-                document.getElementById("Q"+j+"T").innerHTML = quartas[j]
-        } else if (entrada[x] < entrada[y] && entrada[y] != "" && entrada[x] != "") {
-                quartas[j] = vencedores[y]
-                document.getElementById("Q"+j+"T").innerHTML = quartas[j]
-        }
-}
-
-function verificadorQ(x,y,j){
-        enter()
-        if (entradaQ[x] > entradaQ[y] && entradaQ[y] != "" && entradaQ[x] != "" && quartas[x] != undefined && quartas[x] != null) {
-                semifinal[j] = quartas[x]
-                document.getElementById("S"+j+"T").innerHTML = semifinal[j]
-        } else if (entradaQ[x] < entradaQ[y] && entradaQ[y] != "" && entradaQ[x] != "" && quartas[y] != undefined && quartas[y] != null) {
-                semifinal[j] = quartas[y]
-                document.getElementById("S"+j+"T").innerHTML = semifinal[j]
-        }
-}
-
-function verificadorS(x,y,j){
-        enter()
-        if (entradaS[x] > entradaS[y] && entradaS[y] != "" && entradaS[x] != "" && semifinal[x] != undefined && semifinal[x] != null) {
-                lugar3[j] = semifinal[y]
-                final[j] = semifinal[x]
-                document.getElementById("T"+j+"T").innerHTML = lugar3[j]
-                document.getElementById("F"+j+"T").innerHTML = final[j]
-        } else if (entradaS[x] < entradaS[y] && entradaS[y] != "" && entradaS[x] != "" && semifinal[y] != undefined && semifinal[y] != null) {
-                lugar3[j] = semifinal[x]
-                final[j] = semifinal[y]
-                document.getElementById("T"+j+"T").innerHTML = lugar3[j]
-                document.getElementById("F"+j+"T").innerHTML = final[j]
-        }
-}
+document.getElementById("1A").innerHTML = vencedores[0]
+document.getElementById("2A").innerHTML = vencedores[1]
+document.getElementById("1B").innerHTML = vencedores[2]
+document.getElementById("2B").innerHTML = vencedores[3]
+document.getElementById("1C").innerHTML = vencedores[4]
+document.getElementById("2C").innerHTML = vencedores[5]
+document.getElementById("1D").innerHTML = vencedores[6]
+document.getElementById("2D").innerHTML = vencedores[7]
+document.getElementById("1E").innerHTML = vencedores[8]
+document.getElementById("2E").innerHTML = vencedores[9]
+document.getElementById("1F").innerHTML = vencedores[10]
+document.getElementById("2F").innerHTML = vencedores[11]
+document.getElementById("1G").innerHTML = vencedores[12]
+document.getElementById("2G").innerHTML = vencedores[13]
+document.getElementById("1H").innerHTML = vencedores[14]
+document.getElementById("2H").innerHTML = vencedores[15]
 
 function enter() {
         for (let q = 0; q <= 15; q++) {
-                if (document.getElementById("O" + q).value != null && document.getElementById("O" + q).value != "") {
-                        entrada2[q] = document.getElementById("O" + q).value
+                if (document.getElementById("O" + q).value != null) {
+                        entrada[q] = document.getElementById("O" + q).value
                 }
         }
-        for (let a = 0; a <= 7; a++) {
-                if (document.getElementById("Q" + a).value != null && document.getElementById("Q" + a).value != "") {
-                        entradaQ[a] = document.getElementById("Q" + a).value
-                        sessionStorage.setItem("quartas" + a, parseInt(entradaQ[a]))
-                }
+}
+
+document.getElementById("O0").oninput = function () {
+        enter()
+        verificador(0, 3, 0)
+}
+document.getElementById("O1").oninput = function () {
+        enter()
+        verificador(0, 3, 0)
+}
+document.getElementById("O2").oninput = function () {
+        enter()
+        verificador(4, 7, 1)
+}
+document.getElementById("O3").oninput = function () {
+        enter()
+        verificador(4, 7, 1)
+}
+document.getElementById("O4").oninput = function () {
+        enter()
+        verificador(6, 5, 2)
+}
+document.getElementById("O5").oninput = function () {
+        enter()
+        verificador(6, 5, 2)
+}
+document.getElementById("O6").oninput = function () {
+        enter()
+        verificador(2, 1, 3)
+}
+document.getElementById("O7").oninput = function () {
+        enter()
+        verificador(2, 1, 3)
+}
+document.getElementById("O8").oninput = function () {
+        enter()
+        verificador(8, 11, 4)
+}
+document.getElementById("O9").oninput = function () {
+        enter()
+        verificador(8, 11, 4)
+}
+document.getElementById("O10").oninput = function () {
+        enter()
+        verificador(12, 15, 5)
+}
+document.getElementById("O11").oninput = function () {
+        enter()
+        verificador(12, 15, 5)
+}
+document.getElementById("O12").oninput = function () {
+        enter()
+        verificador(10, 9, 6)
+}
+document.getElementById("O13").oninput = function () {
+        enter()
+        verificador(10, 9, 6)
+}
+document.getElementById("O14").oninput = function () {
+        enter()
+        verificador(14, 13, 7)
+}
+document.getElementById("O15").oninput = function () {
+        enter()
+        verificador(14, 13, 7)
+}
+
+function verificador(x, y, j) {
+        if(jogo[j] == null){
+                jogo[j] = 0
+        }else if (jogo[j] == 1) {
+                cont--
+                jogo[j] = 0
+        } else if (jogo[j] == 2) {
+                cont2--
+                jogo[j] = 0
         }
-        for (let a = 0; a <= 3; a++) {
-                if (document.getElementById("S" + a).value != null && document.getElementById("S" + a).value != "") {
-                        entradaS[a] = document.getElementById("S" + a).value
-                        sessionStorage.setItem("terceiro" + a, parseInt(entradaS[a]))
+        if (entrada[x] > entrada[y] && entrada[y] != "" && entrada[x] != "" && jogo[j] == 0) {
+                if (cont >= 7) { cont = 0 }
+                quartas[cont] = vencedores[x]
+                console.log(quartas[cont])
+                cont++
+                jogo[j] = 1
+                for(let g = 0; g <= 7;g++){
+                        if(quartas[g] != null){
+                document.getElementById("Q"+g+"T").innerHTML = quartas[g]
+                        }
                 }
-        }
-        for (let a = 0; a <= 1; a++) {
-                if (document.getElementById("T" + a).value != null && document.getElementById("T" + a).value != "" && document.getElementById("F" + a).value != null && document.getElementById("F" + a).value != "") {
-                        entradaT[a] = document.getElementById("T" + a).value
-                        sessionStorage.setItem("terceiroE" + a, parseInt(entradaT[a]))
-                        entradaF[a] = document.getElementById("F" + a).value
-                        sessionStorage.setItem("final" + a, parseInt(entradaF[a]))
+        } else if (entrada[y] > entrada[x] && entrada[y] != "" && entrada[x] != "" && jogo[j] == 0) {
+                if (cont2 >= 7) { cont2 = 0 }
+                quartas[cont2] = vencedores[y]
+                console.log(quartas[cont2])
+                cont2++
+                jogo[j] = 2
+                for(let g = 0; g <= 7;g++){
+                        if(quartas[g] != null){
+                document.getElementById("Q"+g+"T").innerHTML = quartas[g]
+                        }
                 }
-        }
-        for(let h = 0;h <= 15;h++){
-                sessionStorage.setItem("oitavas" + h, parseInt(entrada2[h]))
-                entrada[sequencia[h]] = parseInt(entrada2[h])
         }
 }
